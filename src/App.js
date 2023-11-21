0e6416087dc4c7dc33e44e37a063f3cbe32b467f
milomir0227 <stojanovicmilomir6@gmail.com>
2023-11-21 07:18:21 -0800

    first commit - tailwind, material environment set

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {ThemeProvider,createTheme} from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { Button } from '@mui/material';
+
 
 function App() {
+  const theme=createTheme({
+    palette:{
+      mode:'light'
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme} >
+      <CssBaseline/>
+      <Button variant='contained' color='primary' >Hello World</Button>
+
+      <div className="bg-gray-200 p-8">
+        <h1 className="text-4xl font-bold text-blue-600">My Tailwind React App</h1>
+        <p className="text-gray-700 mt-4">Welcome to my app using Tailwind CSS!</p>
+      </div>
+    </ThemeProvider>
   );
 }
 
